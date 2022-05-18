7a66536438f0f9aaab4fad0b0ea74a6e26b8df80
James Fincher <heartandkarma@gmail.com>
2022-05-17 23:38:04 -0700

    seed and migration finished

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        milage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        milage: 32312,
+        title: 'clean',
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        milage: 32312,
+    }
+    
+]
+
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
